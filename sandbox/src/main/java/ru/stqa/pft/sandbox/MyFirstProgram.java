07d2ffdb92757460583f5ca3515c0af8cc7e1aec
merpilev_work <bearkill2010@gmail.com>
2020-02-07 17:03:23 +0500

    Создан тест JUnit

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,12 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 	public static void main (String[] args) {
 		System.out.println("Hello, world");
+		System.out.println(2 + 2);
+		Square s = new Square(10);
+		Rectangle r=new Rectangle(4,6);
+
+		System.out.println(s.area());
+		System.out.println(r.area());
 	}
 
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+    public int a;
+
+    public Square(int a) {
+        this.a = a;
+    }
+    public int area (){
+        return this.a*this.a;
+    }
+}
